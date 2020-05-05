1f2d2ac5f6b94be1c240edc003017e29d6d197cd
ChenJY-Count <64063430+ChenJY-Count@users.noreply.github.com>
2020-05-05 15:31:25 +0800

    Add files via upload

---
+++ PYTHON/飞机大战最终代码/bullet.py
@@ -0,0 +1,64 @@
+import pygame
+
+class Bullet1(pygame.sprite.Sprite):
+    def __init__(self, position):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load("images/bullet_11.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = position
+        self.speed = 12
+        self.active = False
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def move(self):
+        self.rect.top -= self.speed
+
+        if self.rect.top < 0:
+            self.active = False
+
+    def reset(self, position):
+        self.rect.left, self.rect.top = position
+        self.active = True
+
+class Bullet2(pygame.sprite.Sprite):
+    def __init__(self, position):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load("images/bullet10.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = position
+        self.speed = 16
+        self.active = False
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def move(self):
+        self.rect.top -= self.speed
+
+        if self.rect.top < 0:
+            self.active = False
+
+    def reset(self, position):
+        self.rect.left, self.rect.top = position
+        self.active = True
+
+class Bullet3(pygame.sprite.Sprite):
+    def __init__(self, position):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load("images/alien_bullet.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = position
+        self.speed = 5
+        self.active = False
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def move(self):
+        self.rect.top += self.speed
+    
+        if self.rect.bottom > 700:
+            self.active = False
+
+    def reset(self, position):
+        self.rect.left, self.rect.top = position
+        self.active = True
